709215a0720190f02fbc30b5ab1cc0e9f0349551
Mendovi <96690983+Mendovi@users.noreply.github.com>
2022-01-06 23:57:44 +0700

    Update wow.js

--- wow.js
+++ wow.js
@@ -2,7 +2,33 @@ let socket = new WebSocket("wss://mendovi.github.io/curly-sniffle/");
 
 
 
-
+function cat_down () {x = 0;
+x = document.getElementById("caat").offsetTop;
+xn = x + 50;
+document.getElementById("caat").style.top = xn+"px"
+socket.send(document.getElementById("caat").style.top);
+}
+function cat_up () {
+x = 0;
+x = document.getElementById("caat").offsetTop;
+xn = x - 50;
+document.getElementById("caat").style.top = xn+"px"
+socket.send(document.getElementById("caat").style.top);
+}
+function cat_left () {
+x = 0;
+x = document.getElementById("caat").offsetLeft;
+xn = x - 50;
+document.getElementById("caat").style.left = xn+"px"
+socket.send(document.getElementById("caat").style.top);
+}
+function cat_right () {
+x = 0;
+x = document.getElementById("caat").offsetLeft;
+xn = x + 50;
+document.getElementById("caat").style.left = xn+"px"
+socket.send(document.getElementById("caat").style.top);
+}
 
 function cam_down () {x = 0;
 x = document.getElementById("cav").offsetTop;
